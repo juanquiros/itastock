ef4341fb884c2e32cc524547f87b59040f8625ac
Juan Augusto Quiros <84255506+juanquiros@users.noreply.github.com>
2025-12-18 18:55:07 -0300

    Add category management for admins

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Business;
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[IsGranted('ROLE_ADMIN')]
+#[Route('/categories', name: 'app_category_')]
+class CategoryController extends AbstractController
+{
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+    }
+
+    #[Route('', name: 'index', methods: ['GET'])]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $business = $this->requireBusinessContext();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findBy(['business' => $business], ['name' => 'ASC']),
+        ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request): Response
+    {
+        $business = $this->requireBusinessContext();
+
+        $category = new Category();
+        $category->setBusiness($business);
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($category);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Categoría creada.');
+
+            return $this->redirectToRoute('app_category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Category $category): Response
+    {
+        $business = $this->requireBusinessContext();
+        $this->denyIfDifferentBusiness($category, $business);
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Categoría actualizada.');
+
+            return $this->redirectToRoute('app_category_index');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'form' => $form,
+            'category' => $category,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Category $category): Response
+    {
+        $business = $this->requireBusinessContext();
+        $this->denyIfDifferentBusiness($category, $business);
+
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $this->entityManager->remove($category);
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Categoría eliminada.');
+        }
+
+        return $this->redirectToRoute('app_category_index');
+    }
+
+    private function requireBusinessContext(): Business
+    {
+        $business = $this->getUser()?->getBusiness();
+
+        if (!$business instanceof Business) {
+            throw new AccessDeniedException('No se puede gestionar categorías sin un comercio asignado.');
+        }
+
+        return $business;
+    }
+
+    private function denyIfDifferentBusiness(Category $category, Business $business): void
+    {
+        if ($category->getBusiness() !== $business) {
+            throw new AccessDeniedException('Solo podés gestionar categorías de tu comercio.');
+        }
+    }
+}
