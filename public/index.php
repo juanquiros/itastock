c21e68b4a2ed51d100503b165b5ad53e547dc9c1
Juan Augusto Quiros <84255506+juanquiros@users.noreply.github.com>
2026-01-05 10:03:13 -0300

    Initialize timezone in public entrypoint

--- public/index.php
+++ public/index.php
@@ -4,6 +4,11 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+$timezone = $_SERVER['APP_TIMEZONE'] ?? $_ENV['APP_TIMEZONE'] ?? null;
+if (is_string($timezone) && $timezone !== '') {
+    date_default_timezone_set($timezone);
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
