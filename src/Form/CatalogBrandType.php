d5b090478439fcb2420c7025a724cace443ee488
Juan Augusto Quiros <84255506+juanquiros@users.noreply.github.com>
2026-01-10 18:48:04 -0300

    Fix slug generation during catalog form submit

--- src/Form/CatalogBrandType.php
+++ src/Form/CatalogBrandType.php
@@ -8,11 +8,18 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CatalogBrandType extends AbstractType
 {
+    public function __construct(private readonly SluggerInterface $slugger)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -39,6 +46,19 @@ class CatalogBrandType extends AbstractType
                 'label' => 'Activo',
                 'required' => false,
             ]);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event): void {
+            $data = $event->getData();
+            if (!is_array($data)) {
+                return;
+            }
+
+            if (!empty($data['name']) && empty($data['slug'])) {
+                $data['slug'] = strtolower($this->slugger->slug((string) $data['name'])->toString());
+            }
+
+            $event->setData($data);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/CatalogCategoryType.php
+++ src/Form/CatalogCategoryType.php
@@ -6,11 +6,18 @@ use App\Entity\CatalogCategory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CatalogCategoryType extends AbstractType
 {
+    public function __construct(private readonly SluggerInterface $slugger)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -21,6 +28,19 @@ class CatalogCategoryType extends AbstractType
                 'label' => 'Activo',
                 'required' => false,
             ]);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event): void {
+            $data = $event->getData();
+            if (!is_array($data)) {
+                return;
+            }
+
+            if (!empty($data['name']) && empty($data['slug'])) {
+                $data['slug'] = strtolower($this->slugger->slug((string) $data['name'])->toString());
+            }
+
+            $event->setData($data);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
